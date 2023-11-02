e4e2446b352ef705c761e8da0ef12ce69724d017
Josed0912 <josephdtowers2@gmail.com>
2023-11-02 14:33:33 -0400

    Added files

---
+++ Assignment3-Torres-JoseDavid/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using Assignment3_Torres_JoseDavid.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3_Torres_JoseDavid.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+
+        public IActionResult Contact()
+        {
+            ViewBag.Header = "Contact Us Today";
+
+            ContactModel contact = new ContactModel();
+
+            contact.CreateDate = DateTime.Now;
+
+            ViewBag.SampleTopics = new List<SelectListItem>
+            {
+            new SelectListItem{
+                Text= "My order",
+                Value = "My order"
+            },
+            new SelectListItem{
+                Text= "Feedback",
+                Value ="Feedback"
+            },
+             new SelectListItem{
+                Text= "Product questions",
+                Value ="Product questions"
+            },
+              new SelectListItem{
+                Text= "Customer service and feedback",
+                Value ="Customer service and feedback"
+            },
+               new SelectListItem{
+                Text= "Technical questions, specifications, geometry, sizing and historical information",
+                Value ="Technical questions, specifications, geometry, sizing and historical information"
+            },
+            new SelectListItem{
+                Text= "Warranty",
+                Value ="Warranty"
+            },
+            new SelectListItem
+            {
+                Text="Registration",
+                Value="Registration"
+            },
+            new SelectListItem
+            {
+                Text="Catalogue requests",
+                Value="Catalogue requests"
+            },
+            new SelectListItem
+            {
+                Text="Owner's manuals",
+                Value="owner's manuals"
+            },
+            new SelectListItem
+            {
+                Text="Media inquiries",
+                Value= "Media inquiries"
+            },
+            new SelectListItem
+            {
+                Text="Sponsorship and donations",
+                Value="Sponsorship and donations"
+            } };
+
+            return View(contact);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Assignment3-Torres-JoseDavid/Models/ContactModel.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Assignment3_Torres_JoseDavid.Models
+{
+    public class ContactModel
+    {
+                                                          
+        public int Id { get; set; } 
+        public DateTime CreateDate { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "First name must be at least 2 characters long")]
+        [RegularExpression("^[^0-9]+$", ErrorMessage = "First name cannot contain numbers.")]
+        [Display(Name = "First Name")]
+
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Last name must be at least 2 characters long")]
+        [RegularExpression("^[^0-9]+$", ErrorMessage = "Last name cannot contain numbers.")]
+        [Display(Name = "Last Name")]
+
+        public string LastName { get; set; }
+
+        //Regex source: https://stackoverflow.com/questions/15774555/efficient-regex-for-canadian-postal-code-function
+
+        [Required]
+        [DataType(DataType.PostalCode)]
+        [RegularExpression(@"/^[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d$/i")]
+
+        public string PostalCode { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@".*@.*\.\w{2,}", ErrorMessage = "Plese enter a valid email address.")]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number.")]
+
+        public string Phone { get; set; }
+
+        [Required]
+
+        public string Topic { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+
+        public string Comments { get; set; }
+       
+    }
+
+   
+}
